402c753221fabd0a6a910b22c35d4f3e31054b08
ONJA <zanabololonaonja@gmail.com>
2025-10-29 17:32:28 +0100

    MGFREG

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,31 @@
+// app/layout.tsx
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
 import "./globals.css";
+import { LanguageProvider } from "@/contexts/LanguageContext";
 
-export const metadata = {
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
   title: "ONG Ndao hifanosika",
 };
 
-type RootLayoutProps = {
-  children: React.ReactNode;
-};
-
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
       <head>
         <link rel="icon" href="/logo.jpg" />
       </head>
       <body className="antialiased">
-        {children}
+        <LanguageProvider>
+        
+            {children}
+         
+        </LanguageProvider>
       </body>
     </html>
   );
