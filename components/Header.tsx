91b22e43608e46af45d3aeb2391da8353acbc60a
ONJA <zanabololonaonja@gmail.com>
2025-11-03 17:42:46 +0100

    globalcsstemoin

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
   // Dictionnaire de traductions
   const translations = {
     fr: {
-      home: "Accueil",
+      home: "Accueilaaaa",
       about: "À propos",
       staff: "Témoignages",
       projects: "Réalisations",
